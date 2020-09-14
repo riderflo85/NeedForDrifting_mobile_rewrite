5241b76f1afc2eaf6ebfaec41b48c3d270a5e476
Florent (Mac) <grenaille.florent85@gmail.com>
2020-09-14 11:24:54 +0200

    Update the DetailServer component

--- src/components/ManageServerEpic/DetailServer/index.js
+++ src/components/ManageServerEpic/DetailServer/index.js
@@ -5,7 +5,8 @@ import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view
 
 
 function DetailServer(props) {
-    const [isLoading, setIsLoading] = useState(false);
+    const [isLoadingAction, setIsLoadingAction] = useState(false);
+    const [isLoadingContent, setIsLoadingContent] = useState(false); // true is default
 
     const server = props.route.params.server[0];
 
@@ -25,67 +26,73 @@ function DetailServer(props) {
 
     return (
         <View style={styles.container}>
-            <ScrollView contentContainerStyle={{flex: 1}} refreshControl={
-                // <RefreshControl refreshing={() => {}} onRefresh={() => {}}/>
-                () => {}
-            }>
-                <KeyboardAwareScrollView>
-                    <View style={[styles.borderAndColorBloc, styles.dataServer]}>
-                        <View style={styles.headerBloc}>
-                            <View style={{justifyContent: 'center', marginRight: 10}}>
-                                <MaterialCommunityIcons name="traffic-light" size={45} color="black"/>
-                                {_stateServer()}
+            {
+                isLoadingContent ?
+                <View style={styles.loadingContent}>
+                    <ActivityIndicator animating size='large'/>
+                </View> :
+                <ScrollView contentContainerStyle={{flex: 1}}>
+                {/* <ScrollView contentContainerStyle={{flex: 1}} refreshControl={
+                    <RefreshControl refreshing={() => {}} onRefresh={() => {}}/>
+                }> */}
+                    <KeyboardAwareScrollView>
+                        <View style={[styles.borderAndColorBloc, styles.dataServer]}>
+                            <View style={styles.headerBloc}>
+                                <View style={{justifyContent: 'center', marginRight: 10}}>
+                                    <MaterialCommunityIcons name="traffic-light" size={45} color="black"/>
+                                    {_stateServer()}
+                                </View>
+                                <Text style={styles.titleServer}>{server.name}</Text>
                             </View>
-                            <Text style={styles.titleServer}>{server.name}</Text>
-                        </View>
-                        <View style={styles.bodyBloc}>
-                            <View style={styles.iconsView}>
-                                <MaterialCommunityIcons style={{marginBottom: 15}} name="file-outline" size={25} color="#053448"/>
-                                <MaterialCommunityIcons style={{marginBottom: 15}} name="car" size={25} color="#053448"/>
-                                <MaterialCommunityIcons style={{marginBottom: 15}} name="go-kart-track" size={25} color="#053448"/>
+                            <View style={styles.bodyBloc}>
+                                <View style={styles.iconsView}>
+                                    <MaterialCommunityIcons style={{marginBottom: 15}} name="file-outline" size={25} color="#053448"/>
+                                    <MaterialCommunityIcons style={{marginBottom: 15}} name="car" size={25} color="#053448"/>
+                                    <MaterialCommunityIcons style={{marginBottom: 15}} name="go-kart-track" size={25} color="#053448"/>
+                                </View>
+                                <View style={styles.dataView}>
+                                    <Text style={{marginBottom: 15, flexWrap: 'wrap'}}>/home/acServer/acServerPro/bin/savegame/cfg/linux/init.cfg</Text>
+                                    <Text style={{marginBottom: 15}}>/home/acServer/acServerPro/bin/savegame/cfg/linux/car.list</Text>
+                                    <Text style={{marginBottom: 15}}>{server.status}</Text>
+                                </View>
                             </View>
-                            <View style={styles.dataView}>
-                                <Text style={{marginBottom: 15, flexWrap: 'wrap'}}>/home/acServer/acServerPro/bin/savegame/cfg/linux/init.cfg</Text>
-                                <Text style={{marginBottom: 15}}>/home/acServer/acServerPro/bin/savegame/cfg/linux/car.list</Text>
-                                <Text style={{marginBottom: 15}}>{server.status}</Text>
+                        </View>
+                        {/* <UpdateTrack updateTracks={this.tracks} server={this.state.server}/> */}
+                        {
+                            isLoadingAction ?
+                            <View style={[styles.borderAndColorBloc, styles.actionServer]}>
+                                <ActivityIndicator size="small"/>
+                            </View> :
+                            <View style={[styles.borderAndColorBloc, styles.actionServer]}>
+                                <TouchableOpacity 
+                                    style={[styles.buttonAction, {backgroundColor: '#16a0b6', borderWidth: 3, borderColor: '#148c9f'}]}
+                                    onPress={() => {}/*_runCommandServer('status')*/}
+                                >
+                                    <MaterialCommunityIcons name="information-outline" size={30} color="white"/>
+                                </TouchableOpacity>
+                                <TouchableOpacity
+                                    style={[styles.buttonAction, {backgroundColor: '#28a745', borderWidth: 3, borderColor: '#23903c'}]}
+                                    onPress={() => {}/*_runCommandServer('start')*/}
+                                >
+                                    <MaterialCommunityIcons name="play-speed" size={30} color="white"/>
+                                </TouchableOpacity>
+                                <TouchableOpacity
+                                    style={[styles.buttonAction, {backgroundColor: '#ffc107', borderWidth: 3, borderColor: '#e6ac00'}]}
+                                    onPress={() => {}/*_runCommandServer('restart')*/}
+                                >
+                                    <MaterialCommunityIcons name="restart" size={30} color="white"/>
+                                </TouchableOpacity>
+                                <TouchableOpacity
+                                    style={[styles.buttonAction, {backgroundColor: '#dc3545', borderWidth: 3, borderColor: '#c32232'}]}
+                                    onPress={() => {}/*_runCommandServer('stop')*/}
+                                >
+                                    <MaterialCommunityIcons name="stop-circle-outline" size={30} color="white"/>
+                                </TouchableOpacity>
                             </View>
-                        </View>
-                    </View>
-                    {/* <UpdateTrack updateTracks={this.tracks} server={this.state.server}/> */}
-                    {
-                        isLoading ?
-                        <View style={[styles.borderAndColorBloc, styles.actionServer]}>
-                            <ActivityIndicator size="small"/>
-                        </View> :
-                        <View style={[styles.borderAndColorBloc, styles.actionServer]}>
-                            <TouchableOpacity 
-                                style={[styles.buttonAction, {backgroundColor: '#16a0b6', borderWidth: 3, borderColor: '#148c9f'}]}
-                                onPress={() => {}/*_runCommandServer('status')*/}
-                            >
-                                <MaterialCommunityIcons name="information-outline" size={30} color="white"/>
-                            </TouchableOpacity>
-                            <TouchableOpacity
-                                style={[styles.buttonAction, {backgroundColor: '#28a745', borderWidth: 3, borderColor: '#23903c'}]}
-                                onPress={() => {}/*_runCommandServer('start')*/}
-                            >
-                                <MaterialCommunityIcons name="play-speed" size={30} color="white"/>
-                            </TouchableOpacity>
-                            <TouchableOpacity
-                                style={[styles.buttonAction, {backgroundColor: '#ffc107', borderWidth: 3, borderColor: '#e6ac00'}]}
-                                onPress={() => {}/*_runCommandServer('restart')*/}
-                            >
-                                <MaterialCommunityIcons name="restart" size={30} color="white"/>
-                            </TouchableOpacity>
-                            <TouchableOpacity
-                                style={[styles.buttonAction, {backgroundColor: '#dc3545', borderWidth: 3, borderColor: '#c32232'}]}
-                                onPress={() => {}/*_runCommandServer('stop')*/}
-                            >
-                                <MaterialCommunityIcons name="stop-circle-outline" size={30} color="white"/>
-                            </TouchableOpacity>
-                        </View>
-                    }
-                </KeyboardAwareScrollView>
-            </ScrollView>
+                        }
+                    </KeyboardAwareScrollView>
+                </ScrollView>
+            }
         </View>
     );
 }
@@ -96,6 +103,11 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: 'rgb(228,228,228)',
         paddingHorizontal: 15,
+    },
+    loadingContent: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
     },
     borderAndColorBloc: {
         backgroundColor: 'white',
